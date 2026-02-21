53d36abc922ff86ddc616e7c1d1413f94a78b78f
Sophia Rainiel Arevalo Aguado <aguadosophiarainiel@gmail.com>
2026-02-20 23:44:37 -0800

    Forgot required main.js comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+// Name: Sophia Rainiel Arevalo Aguado
+// REFRENCE: https://www.youtube.com/watch?v=7GlxzAcs40c
+// Hours: 30-50 hours. My time report from my extention won't load properly
+// Creative Tilt: I almost never draw animals so I used this as an oppurtunity to do so
+// Especially since I had to animate a run cycle, something I have never done successful before
+// with only two legs. I used pixel art and photobashing (the clouds) as it reminds me of
+// my favorite webcomic, Homestuck(TM), that used MSPAint for all its iamges or photoshoped
+// images. 
+
 const config = {
     type: Phaser.AUTO,
     width: 640,
